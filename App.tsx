4ecc5b2f77d1ad393f30db62470a54f60b0c430a
Rik van Duijn <ik@rikvanduijn.nl>
2021-07-06 14:22:18 +0200

    Add apollo client to project

--- App.tsx
+++ App.tsx
@@ -6,22 +6,31 @@ import { Login } from "./src/Login"
 import { NavigationContainer } from "@react-navigation/native"
 import { createDrawerNavigator } from "@react-navigation/drawer"
 import { UserNameProvider } from "./src/components/UserNameContext"
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client"
 
 export default function App() {
   const Drawer = createDrawerNavigator()
 
+  // Initialize Apollo Client
+  const client = new ApolloClient({
+    uri: "https://graphql-pokemon2.vercel.app",
+    cache: new InMemoryCache(),
+  })
+
   return (
     <View style={styles.container}>
-      <UserNameProvider>
-        <NavigationContainer>
-          <Drawer.Navigator>
-            <Drawer.Screen name="login" component={Login} />
-            <Drawer.Screen name="welcome" component={AppContent} />
-          </Drawer.Navigator>
-        </NavigationContainer>
-      </UserNameProvider>
+      <ApolloProvider client={client}>
+        <UserNameProvider>
+          <NavigationContainer>
+            <Drawer.Navigator>
+              <Drawer.Screen name="login" component={Login} />
+              <Drawer.Screen name="welcome" component={AppContent} />
+            </Drawer.Navigator>
+          </NavigationContainer>
+        </UserNameProvider>
 
-      <StatusBar backgroundColor="#fff" style="auto" />
+        <StatusBar backgroundColor="#fff" style="auto" />
+      </ApolloProvider>
     </View>
   )
 }
